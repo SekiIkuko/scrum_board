91f9c36f1401db5c451058fb608bd43ea5e2ae23
SekiIkuko <seki.ikuko@slogan.jp>
2020-08-18 17:01:17 +0900

    indexページを見れるように

--- src/App.vue
+++ src/App.vue
@@ -32,26 +32,20 @@
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">Latest Release!</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <router-view/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
 
   data: () => ({
     //
---
+++ src/views/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>index page</h1>
+    <p>ここで動作確認</p>
+  </div>
+</template>
