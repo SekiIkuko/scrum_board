a4b10b0ec36674b7ab02fefac7a2a308a891a413
SekiIkuko <seki.ikuko@slogan.jp>
2020-08-25 17:05:37 +0900

    付箋用のv-cardを作成

---
+++ src/components/sticky.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-card class="d-inline-block mx-auto">
+    <v-container>
+      <v-row justify="space-between">
+        <v-col cols="auto">
+          <v-textarea
+            height="200"
+            width="200"
+            no-resize
+            label="ストーリー"
+            value="This is clearable text."
+          ></v-textarea>
+        </v-col>
+
+        <v-col
+          cols="auto"
+          class="text-center pl-0"
+        >
+          <v-row
+            class="flex-column ma-0 fill-height"
+            justify="center"
+          >
+            <v-col class="px-0">
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+            </v-col>
+
+            <v-col class="px-0">
+              <v-btn icon>
+                <v-icon>mdi-bookmark</v-icon>
+              </v-btn>
+            </v-col>
+
+            <v-col class="px-0">
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Sticky'
+}
+</script>
--- src/views/index.vue
+++ src/views/index.vue
@@ -2,5 +2,17 @@
   <div class="about">
     <h1>index page</h1>
     <p>ここで動作確認</p>
+    <Sticky />
+    <Sticky />
   </div>
 </template>
+
+<script>
+import Sticky from '@/components/sticky.vue'
+export default {
+  name: 'index',
+  components: {
+    Sticky
+  }
+}
+</script>
